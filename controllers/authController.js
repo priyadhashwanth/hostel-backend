dad0d67af3ba722151b3f65d20f18452c6edfcea
priyadhashwanth <priyadharshini80302gmail.com>
2026-05-04 13:13:43 +0530

    netify

--- controllers/authController.js
+++ controllers/authController.js
@@ -313,7 +313,7 @@ exports.forgotPassword = async (req, res) => {
 
   await user.save();
 
-  const link = `https://marvelous-jalebi-27c0d6.netlify.app/${token}`;
+  const link = `https://tranquil-clafoutis-5cabf6.netlify.app/${token}`;
 
   await sendEmail(
     user.email,
